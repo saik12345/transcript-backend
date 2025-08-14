175cc2286dd004d1089e3330b6526553a6c10475
saik12345 <saikatmukherjee.adm@gmail.com>
2025-08-14 21:09:02 +0530

    updated

--- transcribe.js
+++ transcribe.js
@@ -10,7 +10,7 @@ dotenv.config({});
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 const supadata = new Supadata({ apiKey: process.env.supadata_key });
 const ai = new GoogleGenAI({ apiKey: process.env.gemini_key });
