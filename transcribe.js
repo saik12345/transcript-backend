c7046cd9bf207fc904140e5ce71c7ec7843499a0
saik12345 <111682963+saik12345@users.noreply.github.com>
2026-01-24 20:57:56 +0530

    Update transcribe.js

--- transcribe.js
+++ transcribe.js
@@ -208,7 +208,7 @@ app.post("/streamaitranscript", async (req, res) => {
 
     const stream = await ai.models.generateContentStream({
       model: "gemini-2.5-flash",
-      contents: `${text}.keep Streaming the output but strictly make sure you complete and not stop abruptly. Correct only the grammar part keeping the structure of the sentence same. Don't change the tone or reduce the length of sentences. Don't add anything new. Don't change the number of sentences or reduce the length of the content. Remove any unnecessary words like 'hmm',[sound] etc.keep Streaming the output but make sure you complete and not stop abruptly. Don't use any \n \\ etc.`,
+      contents: `${text}.keep Streaming the output but strictly make sure you complete and not stop abruptly. Correct only the grammar part keeping the structure of the sentence same. Don't change the tone or reduce the length of sentences. Don't add anything new. Don't change the number of sentences or reduce the length of the content. Remove any unnecessary words like 'hmm',[sound] etc.keep Streaming the output but make sure you complete and not stop abruptly. Don't use any \n \\ etc.Mandatorily the output language should be in english and you should translate to english only given the original text provided to you be in some other language. English is mandatory`,
       config: {
         thinkingConfig: {
           thinkingBudget: 0,
